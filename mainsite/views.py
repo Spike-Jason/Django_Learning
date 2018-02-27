35c5f5f882f19f7636479f64feb40851e2a448d2
Spike <xytx1989@vip.qq.com>
2018-02-28 00:39:54 +0800

    数据库取数据显示

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.template.loader import get_template
+from datetime import datetime
 
 # Create your views here.
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    for post in posts:
+        post_title = post.title
+
+    now = datetime.now()
+    html = template.render(locals())
+    return HttpResponse(html)
+'''
+    posts = Post.objects.all()
+    print(posts)
+    post_lists = list()
+    for count, post in enumerate(posts):
+        post_lists.append("No.{}:".format(str(count)) + str(post.title) + "<br>")
+        post_lists.append("<small>" + str(post.body) + "</small><br><br>")
+    return HttpResponse(post_lists)
+'''
